8fa2dc75862590cdad230725b9fa51f2b0859780
rleal1995@hotmail.com <rleal1995@hotmail.com>
2024-01-02 03:54:07 -0500

    add padding to compare button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           setTeam2Logo((results?.match[0] as any)["OPP"]);
           setStats(results);
         }}
-        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:border-gray-700"
+        className="text-white pt-2 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:border-gray-700"
       >
         Compare
       </button>
