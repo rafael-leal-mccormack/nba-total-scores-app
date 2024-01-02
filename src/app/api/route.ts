1918bc5b45983ed4a7e02c82d2bcc97777748e34
rleal1995@hotmail.com <rleal1995@hotmail.com>
2024-01-02 02:55:32 -0500

    push timeout to 60 seconds

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,7 +6,7 @@ import { NextRequest } from "next/server";
 import puppeteer, { Browser, Page } from "puppeteer-core";
 
 // export const runtime = "edge"
-export const maxDuration = 25;
+export const maxDuration = 60;
 
 
 export async function GET(req: NextRequest) {
