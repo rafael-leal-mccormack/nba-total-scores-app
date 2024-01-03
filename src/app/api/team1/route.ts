a731df6d9514aa0f9b1a46d6320e608fe9a4b86f
rleal1995@hotmail.com <rleal1995@hotmail.com>
2024-01-02 21:52:52 -0500

    change max duration for vercel deployment

--- src/app/api/team1/route.ts
+++ src/app/api/team1/route.ts
@@ -6,7 +6,7 @@ import puppeteer, { Browser, Page } from "puppeteer";
 // import puppeteer, { Browser, Page } from "puppeteer-core";
 
 // export const runtime = "edge"
-export const maxDuration = 60;
+export const maxDuration = 10;
 
 
 export async function GET(req: NextRequest) {
