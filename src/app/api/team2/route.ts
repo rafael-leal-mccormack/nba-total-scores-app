da38c22596d0936295a6219c13364654819a9a80
rleal1995@hotmail.com <rleal1995@hotmail.com>
2024-05-26 02:15:20 -0400

    increase duration

--- src/app/api/team2/route.ts
+++ src/app/api/team2/route.ts
@@ -6,7 +6,7 @@ import puppeteer, { Browser, Page } from "puppeteer";
 // import puppeteer, { Browser, Page } from "puppeteer-core";
 
 // export const runtime = "edge"
-export const maxDuration = 30;
+export const maxDuration = 50;
 
 
 export async function GET(req: NextRequest) {
